02002d5bccd01b8049fce05550bcd3fe698eeeec
Team <yehudarothstein@gmail.com>
2024-07-31 14:23:30 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Scouting from './Pages/Scouting/Scouting.jsx';
+import Login from './Pages/Login/Login.jsx';
+import ScoutNav from './Pages/Nav/ScoutNav.jsx';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/Scouting" element={<Scouting />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="/Scouting/ScoutNav" element={<ScoutNav />} />
+
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
