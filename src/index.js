61aa6f0cbb76c35361f75085ba25d3a6b559b42b
Team <yehudarothstein@gmail.com>
2024-07-31 15:49:02 +0300

    Update #1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { UserProvider } from './context/UserContext';
 import reportWebVitals from "./reportWebVitals";
 import {sendToVercelAnalytics} from "./vitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+    <React.StrictMode>
+        <UserProvider>
+            <App />
+        </UserProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-reportWebVitals(sendToVercelAnalytics);
-
-
+reportWebVitals(sendToVercelAnalytics)
